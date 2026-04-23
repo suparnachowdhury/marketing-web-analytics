437e8cb1433d31f0dbca2f82cdd566b8c294cd70
Suparna Chowdhury <suchow32@gmail.com>
2026-04-23 15:59:52 -0400

    Add Question3

---
+++ Marketing _Web_Analytics_Q3.sql
@@ -0,0 +1,8 @@
+/*
+Marketing Director:
+Based on your conversion rate analysis, we bid down gsearch nonbrand on 2012-04-15.
+
+Can you pull gsearch nonbrand trended session volume, by week, 
+to see if the bid changes have caused volume to drop at all?
+
+*/

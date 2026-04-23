763ab8fbaf09a8dde5990831be6f4732e39b8a93
Suparna Chowdhury <suchow32@gmail.com>
2026-04-23 16:29:08 -0400

    Add Solution to Question5

---
+++ Marketing _Web_Analytics_Q5.sql
@@ -0,0 +1,14 @@
+/*
+TRENDING WITH GRANULAR SEGMENTS
+Marketing Director:
+After your device-level analysis of conversion rates, we realized desktop was
+ doing well, so we bid our gsearch nonbrand desktop campaigns up on 2012-05-19.
+
+Could you pull weekly trends for both desktop and mobile so 
+we can see the impact on volume?
+You can use 2012-04-15 until the bid change as a baseline.
+
+*/
+
+    
+

556e4db9818cec16421d2310ab9c8bd903d2f81a
Suparna Chowdhury <suchow32@gmail.com>
2026-04-23 16:08:07 -0400

    Add Solution to Question4

--- Marketing _Web_Analytics_Q4.sql
+++ Marketing _Web_Analytics_Q4.sql
@@ -7,3 +7,17 @@ and the experience was not great.
 Could you pull conversion rates from session to order, by device type?
 
 */
+SELECT
+    w.device_type,
+    COUNT(DISTINCT w.website_session_id) AS sessions,
+    COUNT(DISTINCT o.website_session_id) AS orders,
+    COUNT(DISTINCT o.website_session_id) * 100.0
+        / COUNT(DISTINCT w.website_session_id) AS session_to_order_conv_ratio
+FROM website_sessions w
+LEFT JOIN orders o
+    ON w.website_session_id = o.website_session_id
+WHERE w.created_at < '2012-05-11'
+  AND w.utm_source = 'gsearch'
+  AND w.utm_campaign = 'nonbrand'
+GROUP BY
+    w.device_type;

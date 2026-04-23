1f280eef10f903256880ae86a18e5d3334f3cd4e
Suparna Chowdhury <suchow32@gmail.com>
2026-04-23 16:05:03 -0400

    Add Solution to Question4

---
+++ Marketing _Web_Analytics_Q4.sql
@@ -0,0 +1,9 @@
+/*
+BID OPTIMIZATION FOR PAID TRAFFIC
+Marketing Director:
+I was trying to use our site on my mobile device the other day, 
+and the experience was not great.
+
+Could you pull conversion rates from session to order, by device type?
+
+*/

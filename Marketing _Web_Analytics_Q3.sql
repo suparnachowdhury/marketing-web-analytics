0c7a27b8094660f6ec9c545b127ae459888648c8
Suparna Chowdhury <suchow32@gmail.com>
2026-04-23 16:00:58 -0400

    Add Solution to Question3

--- Marketing _Web_Analytics_Q3.sql
+++ Marketing _Web_Analytics_Q3.sql
@@ -6,3 +6,15 @@ Can you pull gsearch nonbrand trended session volume, by week,
 to see if the bid changes have caused volume to drop at all?
 
 */
+SELECT
+    -- YEAR(created_at) AS yr,
+    -- WEEK(created_at) AS wk,
+    MIN(DATE(created_at)) AS week_start_date,
+    COUNT(website_session_id) AS sessions
+FROM website_sessions
+WHERE created_at < '2012-05-10'
+  AND utm_source = 'gsearch'
+  AND utm_campaign = 'nonbrand'
+GROUP BY
+    YEAR(created_at),
+    WEEK(created_at);

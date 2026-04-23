c174b32282e2d6fd86d09dc92051f4052fa7fac4
Suparna Chowdhury <suchow32@gmail.com>
2026-04-23 16:30:05 -0400

    Add Solution to Question5

--- Marketing _Web_Analytics_Q5.sql
+++ Marketing _Web_Analytics_Q5.sql
@@ -9,6 +9,31 @@ we can see the impact on volume?
 You can use 2012-04-15 until the bid change as a baseline.
 
 */
+SELECT
+    -- YEAR(created_at),
+    -- WEEK(created_at),
+    MIN(DATE(created_at)) AS week_start_date,
+
+    COUNT(DISTINCT CASE 
+        WHEN device_type = 'desktop' 
+        THEN website_session_id 
+        ELSE NULL 
+    END) AS desktop_sessions,
+
+    COUNT(DISTINCT CASE 
+        WHEN device_type = 'mobile' 
+        THEN website_session_id 
+        ELSE NULL 
+    END) AS mobile_sessions
+
+FROM website_sessions
+WHERE created_at BETWEEN '2012-04-15' AND '2012-06-08'
+  AND utm_source = 'gsearch'
+  AND utm_campaign = 'nonbrand'
+
+GROUP BY
+    YEAR(created_at),
+    WEEK(created_at);
 
     
 

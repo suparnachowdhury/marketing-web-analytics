ddb10f83a6cb9ce6567c3251520c4eb2ba7917fd
Suparna Chowdhury <suchow32@gmail.com>
2026-04-23 15:54:12 -0400

    Add Question2

---
+++ Marketing _Web_Analytics_Q2.sql
@@ -0,0 +1,12 @@
+/*
+Marketing Director:
+Sounds like gsearch nonbrand is our major traffic source, but we need to understand 
+if those sessions are driving sales.
+
+Could you please calculate the conversion rate (CVR) from session to order? 
+Based on what we're paying for clicks, we’ll need a CVR of at least 4% to make the numbers work.
+
+If we're much lower, we’ll need to reduce bids. If we’re higher, 
+we can increase bids to drive more volume.
+
+*/

4232cf684ba84ca5b505a3c39df7b2a6baa3affc
Suparna Chowdhury <suchow32@gmail.com>
2026-04-24 11:28:46 -0400

    Update

---
+++ Analyzing Website Performance/02_Finding_top_entry_pages.sql
@@ -0,0 +1,27 @@
+/*
+FINDING TOP ENTRY PAGES
+New Website Manager: 
+Would you be able to pull a list of the top entry pages? I want to confirm 
+where our users are hitting the site.
+If you could pull all entry pages and rank them on entry volume,
+that would be great.
+*/
+
+CREATE TEMPORARY TABLE first_pv_per_session
+SELECT 
+		website_session_id,
+        MIN(website_pageview_id) as first_pv
+FROM website_pageviews
+WHERE created_at < '2012-06-12'
+GROUP BY 
+		website_session_id;
+        
+        
+SELECT
+        pv.pageview_url AS landing_page,
+        COUNT(fs.website_session_id) as sessions_hitting_this_page
+FROM first_pv_per_session fs
+LEFT JOIN website_pageviews  pv
+ON fs.website_session_id = pv.website_session_id
+GROUP BY
+		pv.pageview_url;

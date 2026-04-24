d2b037191d13e9f0eded04fc832fba594f43f273
Suparna Chowdhury <suchow32@gmail.com>
2026-04-24 13:34:01 -0400

    Update

---
+++ Analyzing Website Performance/04_Analyzing_landing_page_test.sql
@@ -0,0 +1,9 @@
+/*
+ANALYZING LANDING PAGE TEST
+Website Manager: 
+Based on your bounce rate analysis, we ran a new custom landing page (/lander-1) 
+in a 50/50 test against the homepage (/home) for our gsearch nonbrand traffic.
+Can you pull bounce rates for the two groups so we can evaluate the new page?
+ Make sure to just look at the time period where /lander-1 was getting traffic, 
+ so that it is a fair comparison.
+*/

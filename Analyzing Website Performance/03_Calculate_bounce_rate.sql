7adcd23686f821b661908a94fa968a76b642890b
Suparna Chowdhury <suchow32@gmail.com>
2026-04-24 12:13:46 -0400

    Update

---
+++ Analyzing Website Performance/03_Calculate_bounce_rate.sql
@@ -0,0 +1,9 @@
+/*
+CALCULATING BOUNCE RATE
+Website Manager: 
+The other day you showed us that all of our traffic is landing on the homepage right now. 
+We should check how that landing page is performing.
+Can you pull bounce rates for traffic landing on the homepage? 
+I would like to see three numbers...Sessions, Bounced Sessions, 
+and % of Sessions which Bounced (aka “Bounce Rate”)
+*/
